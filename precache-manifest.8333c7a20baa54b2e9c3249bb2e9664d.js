801af1493362589810d701508c7c468eab880459
krzysiek <krzychu33b@gmail.com>
2020-05-05 05:06:01 +0200

    Updates

---
+++ precache-manifest.8333c7a20baa54b2e9c3249bb2e9664d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/app-react/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "4833f737412989384827",
+    "url": "/app-react/static/js/runtime~main.af1a7cba.js"
+  },
+  {
+    "revision": "3a74d5b847290eb3f77c",
+    "url": "/app-react/static/js/main.506ad093.chunk.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/app-react/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "3a74d5b847290eb3f77c",
+    "url": "/app-react/static/css/main.4c7dfd5a.chunk.css"
+  },
+  {
+    "revision": "8859c533216e6abdcd424b30257a1591",
+    "url": "/app-react/index.html"
+  }
+];
